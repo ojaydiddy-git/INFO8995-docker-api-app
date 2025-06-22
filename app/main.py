48c8d47dfb70e4551010c12e2c86d7b234d68b8f
Akintunde Stephen Ojediran <steve_ojay@yahoo.com>
2025-06-22 02:33:21 +0000

    ok

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ def get_user(user_id):
 
 @app.route('/')
 def index():
-    return "Flask API is running!"
+    return "Flask API is running, INFO8995, Thank you professor!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
